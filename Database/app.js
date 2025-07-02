4ffa3ad9beca3eeb52e0bc9bc8f0a0d71124a3ea
xrtvann <irvangoblas@gmail.com>
2025-07-02 19:25:02 +0700

    add jquery library

--- Database/app.js
+++ Database/app.js
@@ -1,17 +1,7 @@
-let searchInput = document.getElementById("searchInput");
-let searchButton = document.getElementById("searchButton");
-let tableContainer = document.getElementById("table-container");
-
-searchInput.addEventListener("keyup", function () {
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      tableContainer.innerHTML = xhr.responseText;
-      console.log(searchInput.value);
-    }
-  };
-
-  xhr.open("GET", "ajax/product.php?keyword=" + searchInput.value, true);
-
-  xhr.send();
+$(document).ready(function () {
+  $("#keyword").on("keyup", function () {
+    $("#table-container").load(
+      "ajax/product.php?keyword=" + $("#keyword").val()
+    );
+  });
 });
